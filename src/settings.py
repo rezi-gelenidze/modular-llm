ec3e72aa392a68e1cbf872ab680d4137b4425c9d
rezi-gelenidze <rezi.gelenidze7@gmail.com>
2024-10-26 19:07:25 +0400

    add settings for centralization

---
+++ src/settings.py
@@ -0,0 +1,6 @@
+""" Centralized settings for the project """
+
+# Classification settings
+CLASSIFICATION_THRESHOLD = 0.5
+CLASSIFICATION_LABELS = ["math", "coding", "life advice", "general"]
+CLASSIFICATION_MODEL = "facebook/bart-large-mnli"
